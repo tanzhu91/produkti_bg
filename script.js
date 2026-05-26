2708a4839cc278fa3361a125acd47db095dda1a3
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2026-05-26 17:42:31 +0200

    changes to api key visibility

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
 // Firebase Configuration - ТУК СЛАГАШ ТВОИТЕ КЛЮЧОВЕ ОТ FIREBASE КОНЗОЛАТА
 const firebaseConfig = {
-
-  apiKey: "AIzaSyBXQLq0zmjUEXzKDHUzyASanD3AYzvNyoE",
-
-  authDomain: "local-harvest-bg.firebaseapp.com",
-
-  projectId: "local-harvest-bg",
-
-  storageBucket: "local-harvest-bg.firebasestorage.app",
-
-  messagingSenderId: "963913514176",
-
-  appId: "1:963913514176:web:632c12e367eae8be874125",
-
-  measurementId: "G-KXFY8HZGVK"
-
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
